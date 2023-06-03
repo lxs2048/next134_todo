98cb06fdf31ebd164649b9027eb72bc168979d54
zhangsan <lxs2048@sina.com>
2023-06-03 11:41:31 +0800

    board 拖拽

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,16 +1,98 @@
 'use client'
 import { useBoardStore } from '@/store/BoardStore'
 import { useEffect } from 'react'
+import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd'
+import Column from './Column'
 function Board() {
-  const [board, getBoard] = useBoardStore((state) => [
-    state.board,
-    state.getBoard,
-  ])
+  const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore(
+    (state) => [
+      state.board,
+      state.getBoard,
+      state.setBoardState,
+      state.updateTodoInDB,
+    ]
+  )
   useEffect(() => {
     getBoard()
   }, [getBoard])
-  console.log(board, '数据😎😎😎board')
-  return <div>Board</div>
+  const handleOnDragEnd = (result: DropResult) => {
+    const { type, destination, source } = result
+    // 拖拽到外面
+    if (!destination) return
+    // 处理column drag
+    if (type === 'column') {
+      const entries = Array.from(board.columns.entries())
+      const [removed] = entries.splice(source.index, 1)
+      entries.splice(destination.index, 0, removed)
+      const rearrangedColumns = new Map(entries)
+      setBoardState({
+        ...board,
+        columns: rearrangedColumns,
+      })
+      return
+    }
+    // 处理card drag
+    const columns = Array.from(board.columns)
+    const startColIndex = columns[Number(source.droppableId)]
+    const finishColIndex = columns[Number(destination.droppableId)]
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos,
+    }
+    const finishCol: Column = {
+      id: finishColIndex[0],
+      todos: finishColIndex[1].todos,
+    }
+    if (!startCol || !finishCol) return
+    if (source.index === destination.index && startCol === finishCol) return
+
+    const newTodos = startCol.todos
+    const [todoMoved] = newTodos.splice(source.index, 1)
+    if (startCol.id === finishCol.id) {
+      // 相同task
+      newTodos.splice(destination.index, 0, todoMoved)
+      const newColumns = new Map(board.columns)
+      newColumns.set(startCol.id, {
+        id: startCol.id,
+        todos: newTodos,
+      })
+      setBoardState({ ...board, columns: newColumns })
+    } else {
+      // 其他task
+      const finishTodos = Array.from(finishCol.todos)
+      finishTodos.splice(destination.index, 0, todoMoved)
+
+      const newColumns = new Map(board.columns)
+      newColumns.set(startCol.id, {
+        id: startCol.id,
+        todos: newTodos,
+      })
+      newColumns.set(finishCol.id, {
+        id: finishCol.id,
+        todos: finishTodos,
+      })
+      // handle db
+      updateTodoInDB(todoMoved, finishCol.id)
+      setBoardState({ ...board, columns: newColumns })
+    }
+  }
+  return (
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => (
+          <div
+            className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {Array.from(board.columns.entries()).map(([id, column], index) => (
+              <Column key={id} id={id} todos={column.todos} index={index} />
+            ))}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  )
 }
 
 export default Board
